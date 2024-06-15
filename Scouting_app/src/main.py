ab8db2461b036e3f8eb035cbd784a5b8d5b7456b
Ashesh Lal <87692027+asheshh-lal@users.noreply.github.com>
2024-06-15 15:25:22 -0400

    prompt optimized

--- Scouting_app/src/main.py
+++ Scouting_app/src/main.py
@@ -232,16 +232,105 @@ async def generate_radar_charts(similar_players_cluster_df, player_id):
 
     return radar_charts
 
-def generate_prompt(player_name, chosen_player):
-    prompt = f"Player list = {player_name}. Give short stat report for this player. In the final paragraph give a very short comparison of {player_name} and {chosen_player}."
+def generate_prompt(df_player_description,df_chosen_player_description):
+    ##chosen players are those players that we choose initially
+    # General player stats
+    params = ['Player', 'Pos', 'Squad', 'Age', 'Nation']
+
+    # for player
+    (player_player, player_pos, player_squad, player_age, player_nation) = [df_player_description[param].iloc[0] for param in params]
+
+    # for chosen player
+    (chosen_player_player, chosen_player_pos, chosen_player_squad, chosen_player_age, chosen_player_nation) = [df_chosen_player_description[param].iloc[0] for param in params]
+
+    
+    # List of parameters to extract values for
+    params1 = [
+        'Expected xG', 'Standard Sh', 'Standard SoT%',
+        'Standard Sh/90', 'Aerial Duels Won%', 'Total Att',
+        'Total TotDist', 'Total PrgDist'
+    ]
+
+    # Extract values for the specified parameters from df_player_description using params
+    (player_expected_xg, player_standard_sh, player_standard_sot_percent,
+    player_standard_sh_per_90, player_aerial_duels_won_percent, player_total_att,
+    player_total_tot_dist, player_total_prg_dist) = [df_player_description[param].iloc[0] for param in params1]
+
+    # Extract values for the specified parameters from df_chosen_player_description using params
+    (chosen_player_expected_xg, chosen_player_standard_sh, chosen_player_standard_sot_percent,
+    chosen_player_standard_sh_per_90, chosen_player_aerial_duels_won_percent, chosen_player_total_att,
+    chosen_player_total_tot_dist, chosen_player_total_prg_dist) = [df_chosen_player_description[param].iloc[0] for param in params1]
+    # For defenders
+    params2 = [
+        'Expected xG', 'Tackles Tkl', 'Tackles TklW',
+        'Tackles Def 3rd', 'Tackles Mid 3rd', 'Challenges Tkl%',
+        'Blocks Blocks', 'Blocks Pass'
+    ]
+
+    # Extract values for the specified parameters from df_player_description using params2
+    (player_expected_xg2, player_tackles_tkl2, player_tackles_tklw2,
+    player_tackles_def_3rd2, player_tackles_mid_3rd2, player_challenges_tklp2,
+    player_blocks_blocks2, player_blocks_pass2) = [df_player_description[param].iloc[0] for param in params2]
+
+    # Extract values for the specified parameters from df_chosen_player_description using params2
+    (chosen_player_expected_xg2, chosen_player_tackles_tkl2, chosen_player_tackles_tklw2,
+    chosen_player_tackles_def_3rd2, chosen_player_tackles_mid_3rd2, chosen_player_challenges_tklp2,
+    chosen_player_blocks_blocks2, chosen_player_blocks_pass2) = [df_chosen_player_description[param].iloc[0] for param in params2]
+
+    # For goalkeepers
+    params3 = [
+        "Performance GA", "Performance SoTA", "Performance Saves",
+        "Performance Save%", "Performance CS", "Performance CS%",
+        "Penalty Kicks PKatt", "Penalty Kicks Save%"
+    ]
+
+    # Extract values for the specified parameters from df_player_description using params3
+    (player_performance_ga, player_performance_sota, player_performance_saves,
+    player_performance_save_percent, player_performance_cs, player_performance_cs_percent,
+    player_penalty_kicks_pkatt, player_penalty_kicks_save_percent) = [df_player_description[param].iloc[0] for param in params3]
+
+    # Extract values for the specified parameters from df_chosen_player_description using params3
+    (chosen_player_performance_ga, chosen_player_performance_sota, chosen_player_performance_saves,
+    chosen_player_performance_save_percent, chosen_player_performance_cs, chosen_player_performance_cs_percent,
+    chosen_player_penalty_kicks_pkatt, chosen_player_penalty_kicks_save_percent) = [df_chosen_player_description[param].iloc[0] for param in params3]
+    
+    # For midfielders
+    params4 = [
+        'Expected xA', 'Progression PrgC', 'KP', '1/3', 'PPA',
+        'CrsPA', 'Total Cmp%', 'Total TotDist'
+    ]
+
+    # Extract values for the specified parameters from df_player_description using params
+    (player_expected_xa, player_progression_prgc, player_kp,
+    player_1_3, player_ppa, player_crspa,
+    player_total_cmp, player_total_totdist) = [df_player_description[param].iloc[0] for param in params4]
+
+    # Extract values for the specified parameters from df_chosen_player_description using params
+    (chosen_player_expected_xa, chosen_player_progression_prgc, chosen_player_kp,
+    chosen_player_1_3, chosen_player_ppa, chosen_player_crspa,
+    chosen_player_total_cmp, chosen_player_total_totdist) = [df_chosen_player_description[param].iloc[0] for param in params4]
+
+    if chosen_player_pos in ['FW', 'MF,FW', 'FW,MF']:
+        prompt = f"Player list = . Give short stat report for {player_player}. The player plays in {player_pos} position, is from {player_nation} nation and plays for {player_squad} team and {player_age} years old. Now, These are the stats of {player_player} where Expected goal is {player_expected_xg}, player standard shot is {player_standard_sh}, player standard shot on target percent is {player_standard_sot_percent}, player standard shot per 90 minutes is {player_standard_sh_per_90}, aerial duels won percent is {player_aerial_duels_won_percent}, player total attack is {player_total_att}, total player distance covered {player_total_tot_dist} and finally total player progressive distance covered is {player_total_prg_dist}. Now, these are the stats for {chosen_player_player} where Expected goal is {chosen_player_expected_xg}, player standard shot is {chosen_player_standard_sh}, player standard shot on target percent is {chosen_player_standard_sot_percent}, player standard shot per 90 minutes is {chosen_player_standard_sh_per_90}, aerial duels won percent is {chosen_player_aerial_duels_won_percent}, player total attack is {chosen_player_total_att}, total player distance covered {chosen_player_total_tot_dist} and finally total player progressive distance covered is {chosen_player_total_prg_dist}. In the final paragraph, give a very short comparison of {player_player} and {chosen_player_player}."
+    elif chosen_player_pos in ['DF', 'DF,FW', 'DF,MF', 'FW,DF']:
+        prompt = f"Player list = . Give short stat report for {player_player}. The player plays in {player_pos} position, is from {player_nation} nation and plays for {player_squad} team and {player_age} years old. Now, These are the stats of {player_player} where Expected goal is {player_expected_xg}, player standard shot is {player_standard_sh}, player standard shot on target percent is {player_standard_sot_percent}, player standard shot per 90 minutes is {player_standard_sh_per_90}, aerial duels won percent is {player_aerial_duels_won_percent}, player total attack is {player_total_att}, total player distance covered {player_total_tot_dist} and finally total player progressive distance covered is {player_total_prg_dist}. Now, these are the stats for {chosen_player_player} where Expected goal is {chosen_player_expected_xg}, player standard shot is {chosen_player_standard_sh}, player standard shot on target percent is {chosen_player_standard_sot_percent}, player standard shot per 90 minutes is {chosen_player_standard_sh_per_90}, aerial duels won percent is {chosen_player_aerial_duels_won_percent}, player total attack is {chosen_player_total_att}, total player distance covered {chosen_player_total_tot_dist} and finally total player progressive distance covered is {chosen_player_total_prg_dist}. In the final paragraph, give a very short comparison of {player_player} and {chosen_player_player}."
+    elif chosen_player_pos == 'GK':
+        prompt = f"Player list = . Give short stat report for {player_player}. The player plays in {player_pos} position, is from {player_nation} nation and plays for {player_squad} team and {player_age} years old. Now, These are the stats of {player_player} where Expected goal is {player_expected_xg}, player standard shot is {player_standard_sh}, player standard shot on target percent is {player_standard_sot_percent}, player standard shot per 90 minutes is {player_standard_sh_per_90}, aerial duels won percent is {player_aerial_duels_won_percent}, player total attack is {player_total_att}, total player distance covered {player_total_tot_dist} and finally total player progressive distance covered is {player_total_prg_dist}. Now, these are the stats for {chosen_player_player} where Expected goal is {chosen_player_expected_xg}, player standard shot is {chosen_player_standard_sh}, player standard shot on target percent is {chosen_player_standard_sot_percent}, player standard shot per 90 minutes is {chosen_player_standard_sh_per_90}, aerial duels won percent is {chosen_player_aerial_duels_won_percent}, player total attack is {chosen_player_total_att}, total player distance covered {chosen_player_total_tot_dist} and finally total player progressive distance covered is {chosen_player_total_prg_dist}. In the final paragraph, give a very short comparison of {player_player} and {chosen_player_player}."
+    elif chosen_player_pos in ['MF', 'MF,DF']:
+        prompt = f"Player list = . Give short stat report for {player_player}. The player plays in {player_pos} position, is from {player_nation} nation and plays for {player_squad} team and {player_age} years old. Now, These are the stats of {player_player} where Expected goal is {player_expected_xg}, player standard shot is {player_standard_sh}, player standard shot on target percent is {player_standard_sot_percent}, player standard shot per 90 minutes is {player_standard_sh_per_90}, aerial duels won percent is {player_aerial_duels_won_percent}, player total attack is {player_total_att}, total player distance covered {player_total_tot_dist} and finally total player progressive distance covered is {player_total_prg_dist}. Now, these are the stats for {chosen_player_player} where Expected goal is {chosen_player_expected_xg}, player standard shot is {chosen_player_standard_sh}, player standard shot on target percent is {chosen_player_standard_sot_percent}, player standard shot per 90 minutes is {chosen_player_standard_sh_per_90}, aerial duels won percent is {chosen_player_aerial_duels_won_percent}, player total attack is {chosen_player_total_att}, total player distance covered {chosen_player_total_tot_dist} and finally total player progressive distance covered is {chosen_player_total_prg_dist}. In the final paragraph, give a very short comparison of {player_player} and {chosen_player_player}."
+    else:
+        prompt = "Say sorry"
+
     return prompt
 
-def fetch_gemini_results(player_name, chosen_player):
+
+def fetch_gemini_results(df_player_description,df_chosen_player_description):
     model = genai.GenerativeModel('gemini-pro')
     os.environ['GOOGLE_API_KEY'] = "AIzaSyDqiBvz-_Ng3ZdUl53n1oViYF-tfx18RzM"
     genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
-    
-    prompt = generate_prompt(player_name, chosen_player)
+    chosen_player = df_chosen_player_description['Player']
+    player_name = df_player_description['Player']
+    prompt = generate_prompt(df_player_description,df_chosen_player_description)
     
     response = model.generate_content(prompt, stream=True)
     response_text = ""
@@ -262,14 +351,21 @@ async def view_description(request: Request):
     chosen_player = data.get('player')
 
     async with aiosqlite.connect(DATABASE_URL) as db:
+        #fetch data for player_name
         cursor = await db.execute("SELECT DISTINCT * FROM data WHERE Player = ?", (player_name,))
         rows = await cursor.fetchall()
         columns = [desc[0] for desc in cursor.description]
-        df = pd.DataFrame(rows, columns=columns)
+        df_player_description = pd.DataFrame(rows, columns=columns)
+        
+        # Fetch data for chosen_player
+        cursor = await db.execute("SELECT DISTINCT * FROM data WHERE Player = ?", (chosen_player,))
+        rows = await cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+        df_chosen_player_description = pd.DataFrame(rows, columns=columns)
 
-    if df.empty:
+    if df_player_description.empty:
         description = "<p>No data available for the selected player.</p>"
     else:
-        description = fetch_gemini_results(player_name, chosen_player)
+        description = fetch_gemini_results(df_player_description,df_chosen_player_description)
 
     return JSONResponse(content={"description": description})
